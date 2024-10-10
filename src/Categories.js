40b616e021c99e2f222130189819c9ba1dc4cee9
Nabeelara <nabeela4829.com>
2024-10-10 14:13:38 +0800

    All

--- src/Categories.js
+++ src/Categories.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Categories.css';
 
-const Categories = ({categories, filterItems}) => {
+const Categories = ({filterItems}) => {
+  const categories = ['All', 'Topi', 'Kaos', 'Jas', 'Piyama']
+
   return (
     <div className="btn-container">
       {categories.map( (category, index) => {
         return (
           <button type="button"
           className="filter-btn"
-          key="index"
+          key={index}
           onClick={() => filterItems(category)}
           >
             {category}
